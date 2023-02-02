8527e14d2a0bf427e982dcb4134f6a8ca5cda1aa
linh <14121611ll@gmail.com>
2023-02-02 16:38:54 +0700

    update

--- node/app.js
+++ node/app.js
@@ -1,38 +1,70 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+
+const Blog = require('./models/blog');
+const uri = 'mongodb://localhost:27017/?authSource=admin';
+
+mongoose.set('strictQuery', false);
+mongoose.connect(uri, 
+  {
+    useNewUrlParser: true,
+    user: 'root',
+    pass: '1234'
+  })
+  .then((result) => { 
+    app.listen(3000);
+    console.log('sucess')
+  })
+  .catch((err) => console.log(err)
+);
 
 app.set('view engine', 'ejs');
 
-app.listen(3000);
-
 app.use(express.static('public'));
-
-app.use((req, res, next) => {
-  console.log('new request made:');
-  console.log('host: ', req.hostname);
-  console.log('path: ', req.path);
-  console.log('method: ', req.method);
-  next();
-})
+app.use(express.urlencoded({ extended: true}));
 
 app.get('/', (req, res) => {
-  const blogs = [
-    { title: 'Bolo bala', snippet: 'Toba totba'},
-    { title: 'Xila xilo', snippet: 'sako mola'},
-    { title: 'Ho lo ', snippet: 'Ha la'},
-  ]
-
-  res.render('index', { title: 'Home', blogs});
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About'});
 });
 
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1})
+    .then((result) => {
+      res.render('index', { title: 'All Blogs', blogs: result });
+    })
+})
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render('detail', { blog: result, title: 'Blog detail'})
+    })
+    .catch((result) => {
+      console.log(err);
+    });
+})
+
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog'});
 });
+
+app.post('/blogs', (req, res)=>{
+  const blog = new Blog(req.body);
+
+  blog.save()
+    .then((result) => {
+      res.redirect('/blogs')
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
 
 app.use((req, res) => {
   res.status(404).render('404', { title: '404'});
